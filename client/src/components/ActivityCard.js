6597dee9bc16b28bd686191ce39e34eee07f9224
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-11-28 17:09:59 -0500

    Created Card

--- client/src/components/ActivityCard.js
+++ client/src/components/ActivityCard.js
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import { Card, Image, Button } from 'semantic-ui-react'
+
 
 function ActivityCard({oneActivity}){
     const {name,mood,price,img,description} = oneActivity
@@ -9,22 +11,43 @@ function ActivityCard({oneActivity}){
       }
 
       return(
-        <li className="cards__item">
-          <div className="card">
-            <img onClick={toggleDetails}
-              src={img}
-              alt={name}
-              className="card__image"
-            />
-            <div className="card__content">
-              <div className="card__title">{name}</div>
-              <div className="card__text">{showDetails ? <p >{description}</p> : <p>{mood}{price}</p> }</div>
-              <div className="card__detail">
-              </div>
+        <Card>
+          <Image src={img} wrapped ui={false} onClick={toggleDetails}/>
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <div>
+              {showDetails ? 
+            <Card.Meta>
+              <span className='mood'>Mood: {mood}</span>
+              <span className='price'>Price: {price}</span>
+            </Card.Meta>:
+              <Card.Description>
+                  {description}
+              </Card.Description>
+            }
             </div>
-          </div>
-        </li>
-      );
+            <Button color='green'>Edit</Button>
+            <Button color='red'>Delete</Button>
+          </Card.Content>
+        </Card>
+        )
+      //(
+      //   <li className="cards__item">
+      //     <div className="card">
+      //       <img onClick={toggleDetails}
+      //         src={img}
+      //         alt={name}
+      //         className="card__image"
+      //       />
+      //       <div className="card__content">
+      //         <div className="card__title">{name}</div>
+      //         <div className="card__text">{showDetails ? <p >{description}</p> : <p>{mood}{price}</p> }</div>
+      //         <div className="card__detail">
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </li>
+      // );
 }
 
 
