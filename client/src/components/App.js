8287442b22e8456a52ad66c34d9f30f0c3a7aed5
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-11-28 10:29:42 -0500

    Hooked up Semantic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import { Header } from "semantic-ui-react";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return <Header as='h3' textAlign='center'>
+  Add New Activity!
+</Header>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import 'semantic-ui-css/semantic.min.css'
 import React from "react";
 import App from "./components/App";
 import "./index.css";
